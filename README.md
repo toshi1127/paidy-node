98a984b3d7a2687833d4bab416ba152a05c2a0c9
toshi1127 <toshi.matsumoto.3n@gmail.com>
2024-11-03 16:21:51 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# paidy-node
+
+```
+const api = new DefaultApi(config);
+api.capturePayment({ paymentId, capturePaymentRequest });
+```
---
+++ src/models/index.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CapturePayment201Response';
+export * from './CapturePayment201ResponseBuyer';
+export * from './CapturePaymentRequest';
+export * from './CreatePayment201Response';
+export * from './CreatePayment201ResponseBuyer';
+export * from './CreatePayment201ResponseCapturesInner';
+export * from './CreatePayment201ResponseCapturesInnerItemsInner';
+export * from './CreatePayment201ResponseOrder';
+export * from './CreatePayment201ResponseOrderItemsInner';
+export * from './CreatePayment201ResponseShippingAddress';
+export * from './CreatePaymentRequest';
+export * from './CreatePaymentRequestBuyerData';
+export * from './CreatePaymentRequestOrder';
+export * from './CreatePaymentRequestOrderItemsInner';
+export * from './CreatePaymentRequestShippingAddress';
